921b2713bb76947dc4ea120cc3f6d7706c1c07d7
seoyoun <seoyoun8879@gmail.com>
2025-03-21 11:53:44 +0900

    json으로 요청

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const port = 1234;
 
+app.use(express.json())
+
 app.get('/swag', (req, res) => {
     res.send('get swag');
 });
 
 app.post('/swag', (req, res) => {
-    res.send('post swag');
+    res.send(req.body);
 });
 
 app.listen(port, () => {
