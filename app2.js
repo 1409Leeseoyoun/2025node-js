96155ccf4bddf8b3d0bff04f1ff5ba84f868768a
seoyoun <seoyoun8879@gmail.com>
2025-04-02 10:18:35 +0900

    Read(Get)

--- app2.js
+++ app2.js
@@ -1,20 +1,45 @@
 const express = require('express');
 const path = require('path');
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 const port = 2007;
-app.set('view engine', 'ejs');
 
-// __dirname : 혀재 파일이 속해있는 디랙토리의 절대경로
-// path.join : 운영체제에 맞추어 경로지정자(/ 혹은 \)를 설정해 준다
-app.set('views', path.join(__dirname, 'views'));
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
 
-const travelList = ['뉴욕', '파리', '우리집', '도쿄'];
-
-app.get('/travel', (req, res) => {
-    res.render('travel', {travelList});
+db.connect(err => {
+  if(err) {
+    console.error('MySQL 연결 실패: ', err);
+    return;
+  }
+  console.log('MySQL 열결 성공');
 })
 
+app.set('view engine', 'ejs');
+// __dirname : 현재 파일이 속해있는 디렉토리의 절대경로
+// path.join : 운영체제에 맞추어 경로지정자(\, /)를 설정해준다
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/travel', (req, res) => {
+  const _query = 'SELECT * FROM travellist';
+  db.query(_query, (err, results) => {
+    if(err){
+      console.error('데이터베이스 쿼리 실패', err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    const travelList = results;
+    res.render('travel', {travelList});
+  });
+});
+
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Express server running at http://localhost:${port}`);
 });
