608b21a9f526300c02337c23ca0284ab57a999e7
seoyoun <seoyoun8879@gmail.com>
2025-04-04 12:16:11 +0900

    body-parser 신버전

---
+++ grammer/body-parser.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+const port = 2007;
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.post('/submit', (req, res) => {
+    const {name, year} = req.body;
+    res.send(`Name : ${name}, Year : ${year}`);
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
